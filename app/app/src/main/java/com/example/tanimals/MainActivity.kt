249e28175288b5031978d9c630d0dfaf4b1aee10
Stuxles <stuxles@live.nl>
2019-12-13 12:30:15 +0100

    update dependencies

--- app/app/src/main/java/com/example/tanimals/MainActivity.kt
+++ app/app/src/main/java/com/example/tanimals/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.tanimals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
     }
 }
