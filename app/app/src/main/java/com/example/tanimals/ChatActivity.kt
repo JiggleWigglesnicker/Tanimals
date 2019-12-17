508d5613f7eb7f4c5f3f6cb708476d4eff30edf9
Prime Knight <games.j@live.nl>
2019-12-17 14:55:27 +0100

    chatActivity

---
+++ app/app/src/main/java/com/example/tanimals/ChatActivity.kt
@@ -0,0 +1,17 @@
+package com.example.tanimals
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sendbird.android.SendBird
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import com.sendbird.android.User
+
+
+class ChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
