ed3ca6b5456ff72629aa1abbc11edbc4c96e74ec
Stuxles <stuxles@live.nl>
2019-12-18 11:50:57 +0100

    camera takes pictures and stores in private storage

--- app/app/src/main/java/com/example/tanimals/ProfileActivity.kt
+++ app/app/src/main/java/com/example/tanimals/ProfileActivity.kt
@@ -1,24 +1,82 @@
 package com.example.tanimals
 
+import android.content.Intent
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceFragmentCompat
+import androidx.core.content.FileProvider
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+//        val fbVisionImg = FirebaseVisionImage.fromBitmap(bitmap)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.profile, SettingsFragment())
-            .commit()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
-        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-//            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        // get reference to ImageView
+        val avatarClick = findViewById<ImageView>(R.id.avatar)
+        // set on-click listener
+        avatarClick.setOnClickListener {
+            dispatchTakePictureIntent()
         }
     }
+
+    private var cameraImage: Bitmap? = null
+    val REQUEST_TAKE_PHOTO = 1
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.android.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+                }
+            }
+        }
+    }
+
+    lateinit var currentPhotoPath: String
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
 }
