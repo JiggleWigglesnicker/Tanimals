abe9fb9d88506101fa6c3de7c4615136882023c6
Stuxles <stuxles@live.nl>
2019-12-18 13:11:32 +0100

    choosing pictures works and gives output

--- app/app/src/main/java/com/example/tanimals/ProfileActivity.kt
+++ app/app/src/main/java/com/example/tanimals/ProfileActivity.kt
@@ -1,23 +1,34 @@
 package com.example.tanimals
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.content.res.Resources
 import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.media.Image
+import android.media.ImageReader
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
+import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import java.io.File
 import java.io.IOException
+import java.net.URI
 import java.text.SimpleDateFormat
 import java.util.*
 
 class ProfileActivity : AppCompatActivity() {
+
+    lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -26,18 +37,14 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        // get reference to ImageView
-        val avatarClick = findViewById<ImageView>(R.id.avatar)
+        imageView = findViewById(R.id.avatar)
         // set on-click listener
-        avatarClick.setOnClickListener {
-            dispatchTakePictureIntent()
-        }
     }
 
     private var cameraImage: Bitmap? = null
     val REQUEST_TAKE_PHOTO = 1
 
-    private fun dispatchTakePictureIntent() {
+    fun dispatchTakePictureIntent(v: View) {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
             // Ensure that there's a camera activity to handle the intent
             takePictureIntent.resolveActivity(packageManager)?.also {
@@ -62,8 +69,24 @@ class ProfileActivity : AppCompatActivity() {
         }
     }
 
+    fun selectImage(v: View) {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            imageView.setImageURI(data!!.data)
+            labelBitmapImage()
+        }
+    }
+
     lateinit var currentPhotoPath: String
 
+    @SuppressLint("SimpleDateFormat")
     @Throws(IOException::class)
     private fun createImageFile(): File {
         // Create an image file name
@@ -77,6 +100,28 @@ class ProfileActivity : AppCompatActivity() {
             // Save a file: path for use with ACTION_VIEW intents
             currentPhotoPath = absolutePath
         }
+    }
+
+    fun labelBitmapImage() {
+        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+        val image = FirebaseVisionImage.fromBitmap(bitmap)
+
+        val labeler = FirebaseVision.getInstance().getCloudImageLabeler()
+
+        labeler.processImage(image)
+            .addOnSuccessListener { labels ->
+                for (label in labels) {
+                    val text = label.text
+                    val entityId = label.entityId
+                    val confidence = label.confidence
+                    println(text)
+                }
+            }
+            .addOnFailureListener { e ->
+                // Task failed with an exception
+                // ...
+            }
+
     }
 
 }
