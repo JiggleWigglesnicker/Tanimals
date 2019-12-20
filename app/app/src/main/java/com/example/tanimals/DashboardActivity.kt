6dc50ed81c4d6937bdfc0657061bb7047bbd7cc9
Stuxles <stuxles@live.nl>
2019-12-20 11:54:01 +0100

    read from database and redirect

--- app/app/src/main/java/com/example/tanimals/DashboardActivity.kt
+++ app/app/src/main/java/com/example/tanimals/DashboardActivity.kt
@@ -3,14 +3,27 @@ package com.example.tanimals
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
+        val TAG = "MyMessage"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val db = FirebaseFirestore.getInstance()
+        val user = FirebaseAuth.getInstance().currentUser
         setContentView(R.layout.activity_dashboard)
+
+        val docRef = db.collection("user").document(user!!.uid)
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if(document.data == null) {
+                    startActivity(Intent(this, ProfileActivity::class.java))
+                }
+            }
 
         logoutBut.setOnClickListener{
             FirebaseAuth.getInstance().signOut()
