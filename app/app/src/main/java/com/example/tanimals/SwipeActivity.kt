4523029175c70019f66ad6f96b95fce5ce62559e
Prime Knight <games.j@live.nl>
2020-01-02 15:23:22 +0100

    removed viewpage adapter

--- app/app/src/main/java/com/example/tanimals/SwipeActivity.kt
+++ app/app/src/main/java/com/example/tanimals/SwipeActivity.kt
@@ -2,14 +2,7 @@ package com.example.tanimals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
-import androidx.viewpager.widget.ViewPager
+
 
 
 class SwipeActivity : AppCompatActivity() {
@@ -18,58 +11,5 @@ class SwipeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_swipe)
 
-    }
-}
-
-class CollectionFragment : Fragment() {
-    private lateinit var demoCollectionPagerAdapter: CollectionPagerAdapter
-    private lateinit var viewPager: ViewPager
-
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.activity_swipe, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        demoCollectionPagerAdapter = CollectionPagerAdapter(childFragmentManager)
-        viewPager = view.findViewById(R.id.pager)
-        viewPager.adapter = demoCollectionPagerAdapter
-    }
-}
-
-
-class CollectionPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
-
-    override fun getCount(): Int  = 100
-
-    override fun getItem(i: Int): Fragment {
-        val fragment = ObjectFragment()
-        fragment.arguments = Bundle().apply {
-            putInt(ARG_OBJECT, i + 1)
-        }
-        return fragment
-    }
-
-    override fun getPageTitle(position: Int): CharSequence {
-        return "OBJECT ${(position + 1)}"
-    }
-}
-
-private const val ARG_OBJECT = "object"
-
-class ObjectFragment : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        arguments?.takeIf { it.containsKey(ARG_OBJECT) }?.apply {
-            val textView: TextView = view.findViewById(android.R.id.text1)
-            textView.text = getInt(ARG_OBJECT).toString()
-        }
     }
 }
