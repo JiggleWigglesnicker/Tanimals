55ea282c3af30d6d9a4f24b3bdb37d74a398edca
Stuxles <stuxles@live.nl>
2020-01-09 10:19:55 +0100

    add empty function

---
+++ app/app/src/main/java/com/example/tanimals/ChatListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tanimals
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_list)
+    }
+}
