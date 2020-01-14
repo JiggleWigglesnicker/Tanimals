d9f3ba156c732a1f5fa9243a3af87b27d9e0442e
Stuxles <stuxles@live.nl>
2020-01-14 12:08:09 +0100

    comment code

--- app/app/src/main/java/com/example/tanimals/ChatListActivity.kt
+++ app/app/src/main/java/com/example/tanimals/ChatListActivity.kt
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import kotlin.collections.ArrayList
 
 class ChatListActivity : AppCompatActivity() {
@@ -14,12 +17,11 @@ class ChatListActivity : AppCompatActivity() {
 
     private val user = FirebaseAuth.getInstance().currentUser
     private val db = FirebaseFirestore.getInstance()
-    private val matchArray: ArrayList<String> = ArrayList()
+    private var matchArray: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_list)
-
 
         //Retrieve user matches
         val docRef = db.collection("match").document(user!!.uid)
@@ -30,32 +32,35 @@ class ChatListActivity : AppCompatActivity() {
                     //iterate over matches where k is match userID, v is liked boolean(True is liked, False is disliked)
                     document.data!!.forEach { (k, v) ->
                         //continue if user liked
-                        if (v == true) {
-                            // Gets the matches of the person,to compare if the both have matched
-                            val docRef = db.collection("match").document(k)
-                            docRef.get()
-                                .addOnSuccessListener { document ->
-                                    //continue if there are matches
-                                    if (document != null) {
-                                        //checks if user is in the list
-                                        if (document.data?.get(user.uid) != null) {
-                                            //checks if user is liked
-                                            if (document.data?.get(user.uid) == true) {
-                                                //if both have matched add user to the "matchArray" array
-                                                matchArray.add(k)
-                                            }
-                                        }
-
-                                    } else {
-                                        Log.d(TAG, "No such document")
-                                    }
-                                }
-                                .addOnFailureListener { exception ->
-                                    Log.d(TAG, "get failed with ", exception)
-                                }
-                        }
+                            getMatches(v,k)
                     }
                     println(matchArray)
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
+    }
+
+
+    private fun getMatches(v:Any, k:String){
+        // Gets the matches of the person,to compare if the both have matched
+        val docRefMatch = db.collection("match").document(k)
+        docRefMatch.get()
+            .addOnSuccessListener { document ->
+                //continue if there are matches
+                if (document != null) {
+                    //checks if user is in the list
+                    if (document.data?.get(user!!.uid) != null) {
+                        //checks if user is liked
+                        if (document.data?.get(user!!.uid) == true) {
+                            //if both have matched add user to the "matchArray" array
+                            matchArray.add(k)
+                        }
+                    }
+
                 } else {
                     Log.d(TAG, "No such document")
                 }
