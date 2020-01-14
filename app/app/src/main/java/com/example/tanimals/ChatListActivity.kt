d8bea348f572c9aee9f01d4ce448bd9adb4fa09a
Stuxles <stuxles@live.nl>
2020-01-14 11:09:22 +0100

    comment code

--- app/app/src/main/java/com/example/tanimals/ChatListActivity.kt
+++ app/app/src/main/java/com/example/tanimals/ChatListActivity.kt
@@ -21,32 +21,41 @@ class ChatListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_chat_list)
 
 
+        //Retrieve user matches
         val docRef = db.collection("match").document(user!!.uid)
         docRef.get()
             .addOnSuccessListener { document ->
+                //continue if there are matches
                 if (document != null) {
+                    //iterate over matches where k is match userID, v is liked boolean(True is liked, False is disliked)
                     document.data!!.forEach { (k, v) ->
+                        //continue if user liked
                         if (v == true) {
-                            // Get matches for matched
+                            // Gets the matches of the person,to compare if the both have matched
                             val docRef = db.collection("match").document(k)
                             docRef.get()
                                 .addOnSuccessListener { document ->
+                                    //continue if there are matches
                                     if (document != null) {
+                                        //checks if user is in the list
                                         if (document.data?.get(user.uid) != null) {
+                                            //checks if user is liked
                                             if (document.data?.get(user.uid) == true) {
+                                                //if both have matched add user to the "matchArray" array
                                                 matchArray.add(k)
                                             }
                                         }
+
                                     } else {
                                         Log.d(TAG, "No such document")
                                     }
-
                                 }
                                 .addOnFailureListener { exception ->
                                     Log.d(TAG, "get failed with ", exception)
                                 }
                         }
                     }
+                    println(matchArray)
                 } else {
                     Log.d(TAG, "No such document")
                 }
