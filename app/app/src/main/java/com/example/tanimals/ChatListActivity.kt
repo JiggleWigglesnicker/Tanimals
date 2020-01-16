eb15f587d6d70ee8e732010198117a152da47251
Stuxles <stuxles@live.nl>
2020-01-16 15:43:41 +0100

    empty chatlistactivity for rework

--- app/app/src/main/java/com/example/tanimals/ChatListActivity.kt
+++ app/app/src/main/java/com/example/tanimals/ChatListActivity.kt
@@ -3,11 +3,18 @@ package com.example.tanimals
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.functions.FirebaseFunctionsException
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.lang.Thread.sleep
 import kotlin.collections.ArrayList
 
 class ChatListActivity : AppCompatActivity() {
@@ -16,57 +23,10 @@ class ChatListActivity : AppCompatActivity() {
     }
 
     private val user = FirebaseAuth.getInstance().currentUser
-    private val db = FirebaseFirestore.getInstance()
-    private var matchArray: ArrayList<String> = ArrayList()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_list)
-
-        //Retrieve user matches
-        val docRef = db.collection("match").document(user!!.uid)
-        docRef.get()
-            .addOnSuccessListener { document ->
-                //continue if there are matches
-                if (document != null) {
-                    //iterate over matches where k is match userID, v is liked boolean(True is liked, False is disliked)
-                    document.data!!.forEach { (k, v) ->
-                        //continue if user liked
-                            getMatches(v,k)
-                    }
-                    println(matchArray)
-                } else {
-                    Log.d(TAG, "No such document")
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.d(TAG, "get failed with ", exception)
-            }
-    }
-
-
-    private fun getMatches(v:Any, k:String){
-        // Gets the matches of the person,to compare if the both have matched
-        val docRefMatch = db.collection("match").document(k)
-        docRefMatch.get()
-            .addOnSuccessListener { document ->
-                //continue if there are matches
-                if (document != null) {
-                    //checks if user is in the list
-                    if (document.data?.get(user!!.uid) != null) {
-                        //checks if user is liked
-                        if (document.data?.get(user!!.uid) == true) {
-                            //if both have matched add user to the "matchArray" array
-                            matchArray.add(k)
-                        }
-                    }
-
-                } else {
-                    Log.d(TAG, "No such document")
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.d(TAG, "get failed with ", exception)
-            }
     }
 }
